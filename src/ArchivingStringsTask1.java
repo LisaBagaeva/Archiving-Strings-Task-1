adf5cb91f8e24d0ce66bf49d379c57a4888305b6
Lizzi <Lizzi>
2017-02-07 13:14:06 +0300

    Add update ArchivingStringsTask1

--- src/ArchivingStringsTask1.java
+++ src/ArchivingStringsTask1.java
@@ -1,41 +1,47 @@
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArchivingStringsTask1 {
 	public static void archive(File fileIn) {
+		List<String> outStrings = new ArrayList<>();
 		String out = "";
-		String line;
 		int value = 0;
 		Character lastChar = 0;
+		String line = "";
 		try (BufferedReader br = new BufferedReader(new FileReader(fileIn))) {
-			while ((line = br.readLine()) != null)
-				for (Character ch : line.toCharArray())
+			while ((line = br.readLine()) != null) {
+				for (Character ch : line.toCharArray()) {
 					if (ch != ' ') {
 						if (lastChar == 0)
 							lastChar = ch;
 						if (lastChar == ch)
 							value = value + 1;
-						 else {
+						else {
 							out = out.concat(lastChar.toString() + value);
 							lastChar = ch;
 							value = 1;
 						}
 					}
-			out = out.concat(lastChar.toString() + value);
-
+				}
+				outStrings.add(out);
+				out = "";
+			}
+			int lastIndex = outStrings.size() - 1;
+			outStrings.set(lastIndex, outStrings.get(lastIndex).concat(lastChar.toString() + value));
 		} catch (IOException ex) {
 			System.out.println(ex.getMessage());
 		}
-
 		File fileOut = new File(fileIn.getParent(), "FileOut.txt");
 		try (BufferedWriter wr = new BufferedWriter(new FileWriter(fileOut))) {
-			wr.write(out);
+			for (String s : outStrings)
+				wr.write(s + '\n');
 		} catch (IOException ex) {
 			System.out.println(ex.getMessage());
 		}
